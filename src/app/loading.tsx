a7c94f2e5cab02cf5e161f9e64eb9f692da11257
abrasumente999 <katieoliviaholliday@gmail.com>
2023-03-01 16:50:52 +0000

    add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className=" flex justify-center content-center h-full w-full text-center my-auto p-52">
+      <h1 className=" font-bold text-xl">Loading...</h1>
+    </div>
+  );
+}
