e33137ff3d7c2ee6202ba365fb31a1216555691f
abrasumente999 <katieoliviaholliday@gmail.com>
2023-03-01 16:53:36 +0000

    add pagination component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-
+import { Search } from "./components/SearchBar";
 import PokemonList from "./components/PokemonList";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,7 +7,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="flex flex-col mx-5 md:mx-20 justify-between">
-      <h1 className="text-xl font-bold text-gray-800">Pokédex</h1>
       {/* @ts-expect-error Server Component */}
       <PokemonList />
     </main>
